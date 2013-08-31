9c0df483f308c8e6dc81d424c3fd4f42ba698d36
adis1313 <adis1313@ubuntu.(none)>
2013-08-31 15:31:50 +0200

    Initial commit

---
+++ arch/arm/mach-s5pv210/include/mach/regs-sys.h
@@ -0,0 +1,27 @@
+/* arch/arm/mach-s5pv210/include/mach/regs-sys.h
+ *
+ * Copyright (c) 2010 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com/
+ *
+ * S5PV210 - System registers definitions
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+#define S5PV210_USB_PHY_CON	(S3C_VA_SYS + 0xE80C)
+#define S5PV210_USB_PHY0_EN	(1 << 0)
+#define S5PV210_USB_PHY1_EN	(1 << 1)
+
+/* compatibility defines for s3c-hsotg driver */
+#define S3C64XX_OTHERS		S5PV210_USB_PHY_CON
+#define S3C64XX_OTHERS_USBMASK	S5PV210_USB_PHY0_EN
+
+#define S5PV210_TZPC0 0xF1500000
+#define S5PV210_TZPC1 0xFAD00000
+#define S5PV210_TZPC2 0xE0600000
+#define S5PV210_TZPC3 0xE1C00000
+#define S5PV210_TZPC_DECPROT0SET 0x804
+#define S5PV210_TZPC_DECPROT1SET 0x810
+#define S5PV210_TZPC_DECPROT2SET 0x81C
